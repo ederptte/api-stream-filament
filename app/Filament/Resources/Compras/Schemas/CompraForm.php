6e709e5d21059140754340605d301a13b2b48af0
ederptte <ederptte25@gmail.com>
2026-07-28 17:19:03 -0500

    Agregar estado renovada a la compra

--- app/Filament/Resources/Compras/Schemas/CompraForm.php
+++ app/Filament/Resources/Compras/Schemas/CompraForm.php
@@ -37,7 +37,6 @@ class CompraForm
                     ->default(1),
                 Select::make('dias_duracion')
                     ->required()
-                    ->numeric()
                     ->integer()
                     ->minValue(1)
                     ->default(30),
